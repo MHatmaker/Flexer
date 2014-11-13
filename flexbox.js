2632b2dfd311fc1995eb48b7f5d807720e87e0b9
MHatmaker <Michael.Hatmaker@gmail.com>
2014-11-13 13:00:11 -0700

    Added list of presidents.
    inner table height is a percentage of outer table height.

--- flexbox.js
+++ flexbox.js
@@ -1,18 +1,25 @@
 var app = angular.module('plunker', []);
 
+var presidentList = [
+  'Washington', 'Adams', 'Jefferson', 'Madison','Monroe', 'JQAdams', 'Jackson', 'Van Buren',
+  'WHHarrison', 'Tyler', 'Polk', 'Taylor', 'Fillmore', 'Pierce', 'Buchannon', 'Lincoln', 'AJohnson', 'Grant',
+  'Hayes', 'Garfield', 'Arthur', 'Cleveland22', 'BHarrison', 'Cleveland24', 'McKinnely'];
+  
+var ulRatio = 0.9;
+  
+
 app.controller('MainCtrl', function($scope) {
   $scope.Header = "Here we are";
   $scope.Expand = "Show Plugin";
   $scope.VerbVis = "none";
   $scope.outerTblHeight = 400;
-  $scope.innerTblHeight =330;
+  $scope.innerTblHeight = $scope.outerTblHeight *ulRatio;
   
-  $scope.presidents = {
-  'Washington', 'Adams', 'Jefferson', 'Madison','Monroe', 'JQAdams', 'Jackson', 'Van Buren',
-  'WHHarrison', 'Tyler', 'Polk', 'Taylor', 'Fillmore', 'Pierce', 'Buchannon', 'Lincoln', 'AJohnson', 'Grant'];
+  $scope.presidents = presidentList;
   
   $scope.onExpClick = function(){
       $scope.VerbVis = $scope.Expand == "Show Plugin" ? "inline" : "none";
       $scope.Expand = $scope.Expand == "Show Plugin" ? "Hide Plugin" : "Show Plugin";
+      $scope.innerTblHeight = $scope.outerTblHeight * ulRatio;
   };
 });
