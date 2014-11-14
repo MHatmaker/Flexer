3094e6b48391fcb70e9c367a4664ba094f61245b
MHatmaker <Michael.Hatmaker@gmail.com>
2014-11-14 10:58:39 -0700

    Started replacing <p> and <hX> with divs with embedded text, controlling fonts, borders in div css.
    Major improvements in top cell header and body.

--- flexbox.js
+++ flexbox.js
@@ -3,13 +3,14 @@ var app = angular.module('plunker', []);
 var presidentList = [
   'Washington', 'Adams', 'Jefferson', 'Madison','Monroe', 'JQAdams', 'Jackson', 'Van Buren',
   'WHHarrison', 'Tyler', 'Polk', 'Taylor', 'Fillmore', 'Pierce', 'Buchannon', 'Lincoln', 'AJohnson', 'Grant',
-  'Hayes', 'Garfield', 'Arthur', 'Cleveland22', 'BHarrison', 'Cleveland24', 'McKinnely'];
+  'Hayes', 'Garfield', 'Arthur', 'Cleveland22', 'BHarrison', 'Cleveland24', 'McKinley', 'TRoosevelt', 'Taft',
+  'Wilson', 'Harding', 'Coolidge', 'Hoover', 'FRoosevelt', 'Truman', 'Eisenhower', 'Kennedy', 'LJohnson'];
   
 var ulRatio = 0.9;
   
 
 app.controller('MainCtrl', function($scope) {
-  $scope.Header = "Here we are";
+  $scope.Header = "Site Exerciser";
   $scope.Expand = "Show Plugin";
   $scope.VerbVis = "none";
   $scope.outerTblHeight = 400;
